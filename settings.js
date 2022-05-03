c9fa3278efb35c4f699d853a49c6957f84814cda
iStvnbotz <104764934+iStvnbotz@users.noreply.github.com>
2022-05-03 19:53:01 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,74 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Apikey kalian', // Masukin apikey kalian
+}
+
+// Other
+global.owner = ['6287814897547']
+global.premium = ['6287814897547']
+global.ownernomer = '6287814897547'
+global.ownername = 'Steven Lim'
+global.botname = 'iStvnBotz-Md'
+global.footer = 'Steven Lim'
+global.ig = 'https://instagram.com/zyn.mood_'
+global.email = 'zyens80@gmail.com'
+global.region = 'Indonesia'
+global.sc = 'https://www.youtube.com/channel/UCZ_GA4pb1Hjsh9Bm9Ul7LgQ'
+global.myweb = 'https://api-riychdwayne.herokuap.com'
+global.packname = 'iStvnBotz-Md'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'chika'
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Process...',
+    error: 'Fitur sedang error!',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+global.asek = fs.readFileSync('./image/velo.mp4')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
